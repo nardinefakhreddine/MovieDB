446322c54c578d19d717ea1aefda2437783c381b
Nardine <nardinefakhreddine@gmail.com>
2020-10-24 11:44:11 +0300

    nardine-step3

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ app.get('/', function (req, res) {
   app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+app.get('/test', function (req, res) {
+  res.send('{status:200, message:"ok"}'  )
+})
+var today = new Date();
+var time = today.getHours() + ":" + today.getSeconds();
+app.get('/time', function (req, res) {
+  res.send('{status:200, message:'+time+'}'  )
+})
