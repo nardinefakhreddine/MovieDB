e6d9356b561ee0fd4938cc0564c263249bbe4545
Nardine <nardinefakhreddine@gmail.com>
2020-10-24 18:27:24 +0300

    nardine-step5

--- index.js
+++ index.js
@@ -30,3 +30,31 @@ app.get('/search', (req, res)=>{
     res.send('{status:500, error:true, message:you have to provide a search}');
   }
 })
+const movies = [
+  { title: 'Jaws', year: 1975, rating: 8 },
+  { title: 'Avatar', year: 2009, rating: 7.8 },
+  { title: 'Brazil', year: 1985, rating: 8 },
+  { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+//step 5
+app.get('/movies/create', function (req, res) {
+  res.send('{status:200, message:create}'); //or use req.param('id')
+})
+app.get('/movies/read/',(req,res) => {
+  res.send({status:200, data:movies})
+})
+app.get('/movies/update', function (req, res) {
+  res.send('{status:200, message:update}'); //or use req.param('id')
+})
+app.get('/movies/delete', function (req, res) {
+  res.send('{status:200, message:delete}'); //or use req.param('id')
+})
+app.get('movies/add', function (req, res) {
+  res.send('{status:200, message:add}'); //or use req.param('id')
+})
+app.get('movies/get', function (req, res) {
+  res.send('{status:200, message:get}'); //or use req.param('id')
+})
+app.get('/movies/edit', function (req, res) {
+  res.send('{status:200, message:edit}'); //or use req.param('id')
+})
