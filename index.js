19514017507c3a297fa0ceb0a276071f260f933b
Nardine <nardinefakhreddine@gmail.com>
2020-10-25 17:37:04 +0200

    step-12

--- index.js
+++ index.js
@@ -1,12 +1,110 @@
 var express = require('express')
-
+const bodyParser= require('body-parser')
+var ObjectId = require('mongodb').ObjectID;
 const app = express();
 const port = 3000;
+const MongoClient = require('mongodb').MongoClient
+//reference https://zellwk.com/blog/crud-express-mongodb/
+//mongo
+var mongo='mongodb+srv://Movies-DB:11111@cluster0.ns0to.mongodb.net/Movies-db?retryWrites=true&w=majority';
+
+  // I chose name my new database movie
+  MongoClient.connect(mongo, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('movie')
+    const quotesCollection = db.collection('quotes')
+    app.post('/movies/add', (req, res) => {
+      //insert part
+      var t=req.query.title;
+      var y=req.query.year;
+      var r=req.query.rating;
+      quotesCollection.insertOne({ title: t, year: y, rating: r })
+         .then(result => {
+          res.status(200).send(result);
+      })
+        .catch(error => console.error(error))
+
+    })
+    //update data
+    app.put('/movies/update/:id',(req,res)=>{
+      var t=req.query.title;
+      var y=req.query.year;
+      var r=req.query.rating;
+      if(y.length==4)
+            quotesCollection.findOneAndUpdate(
+                { _id: ObjectId(req.params.id) }
+                ,
+                {
+                  $set: {
+                    year: y
+                  }
+                },
+                {
+                  upsert: true
+                }
+              )
+               
+                
+            if(t!=undefined && t!="")
+            quotesCollection.findOneAndUpdate(
+                { _id: ObjectId(req.params.id) }
+                ,
+                {
+                  $set: {
+                    title: t
+                  }
+                },
+                {
+                  upsert: true
+                }
+              )
+                
+                
+
+            if(r!=undefined && r!="")
+            quotesCollection.findOneAndUpdate(
+                { _id: ObjectId(req.params.id) }
+                ,
+                {
+                  $set: {
+                    rating: r
+                  }
+                },
+                {
+                  upsert: true
+                }
+              )
+              res.status(200).send("succes updated");    
+      })
+      //delete dataa
+      app.delete('/movies/delete/:id',(req,res) => {
+      quotesCollection.findOneAndDelete(
+        { _id: ObjectId(req.params.id) }
+      )
+      .then(result => {
+        res.status(200).send(result);
+    })
+        .catch(error => console.error(error))
+  })
+    //display data
+    app.get('/movies/read/',(req,res) => {
+     db.collection('quotes').find().toArray()
+     .then(results => {
+      res.status(200).send(results)
+      })
+    })
+  })  
+  .catch(console.error)
+// Make sure you place body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
  res.send('ok')
 })
-
+/*app.get('/movies/read/',(req,res) => {
+  res.send({status:200, data:movies})
+})*/
   app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`)
 })
@@ -43,9 +141,7 @@ const movies = [
 app.post('/movies/create', function (req, res) {
   res.send('{status:200, message:create}'); //or use req.param('id')
 })
-app.get('/movies/read/',(req,res) => {
-  res.send({status:200, data:movies})
-})
+
 // start Read part
 app.get('/movies/read/:order',(req,res) => {
  var order=req.params.order;
@@ -83,7 +179,7 @@ app.get('/movies/read/id/:ID',(req,res) => {
   }
 })
 
-
+/*
 app.put('/movies/update/:ID',(req,res) => {
   let id= req.params.ID
   let title = req.query.title
@@ -108,10 +204,11 @@ res.send({status:200, message: movies})
   }
 
 })
- 
-
+ */
+/*
 app.delete('/movies/delete/:ID',(req,res) => {
   var id = req.params.ID
+  
   if (id> 0 && id < movies.length ) {
       movies.splice(id-1, 1)
       res.send({status:200, message: movies})
@@ -124,7 +221,7 @@ app.post('/movies/add',(req,res) => {
   var title = req.query.title
   var year= req.query.year
   var rating = req.query.rating
-  if( year == undefined || year.length > 4 || isNaN(year) || title == undefined) {
+  if( year == undefined || year.length !=4  || isNaN(year) || title == undefined) {
       res.send({status:403, error:true, message:'you cannot create a movie without providing a title and a year'})
   }
   if (rating == "" || rating== undefined) {
@@ -132,7 +229,7 @@ app.post('/movies/add',(req,res) => {
   }
   movies.push({title: title, year: year, rating: rating})
       res.send({status:200, data:movies})
-  })
+  })*/
 app.get('movies/get', function (req, res) {
   res.send('{status:200, message:get}'); //or use req.param('id')
 })
